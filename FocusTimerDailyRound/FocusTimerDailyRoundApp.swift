f4d4fb8dfc92d5def24125a5e1615b5fa07a5150
Jatin Foujdar <jatin321001@gmail.com>
2026-06-11 14:57:02 +0530

    Initial Commit

---
+++ FocusTimerDailyRound/FocusTimerDailyRoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerDailyRoundApp.swift
+//  FocusTimerDailyRound
+//
+//  Created by jatin foujdar on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerDailyRoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
